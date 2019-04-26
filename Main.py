25a42c4ac805707b6e9082de9afae94dbd15f0b1
GabrielaVidal7 <ga28bvidal@gmail.com>
2019-04-26 11:49:41 -0300

    OE Migo Gato ta me ajudando

---
+++ Main.py
@@ -0,0 +1,24 @@
+x=5
+print x
+txt='Hello'
+txt_2=' World!'
+print(txt+str(x)+txt_2)
+if x<6:
+	x**=2
+
+	print x
+elif x>6:
+	x=num
+
+try:
+	print(x/0)
+except:
+	print('ops!')
+
+list=[1,2,3,4,5]
+
+for num in list:
+	print num
+
+for x in range(1,10):
+	pass
